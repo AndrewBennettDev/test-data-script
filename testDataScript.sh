b932bef2af4e0ead274fa245de9fcf7c7d112e6c
andrew.bennett <andrew.bennett@bettercloud.com>
2022-12-16 11:26:20 -0500

    add formatting

--- testDataScript.sh
+++ testDataScript.sh
@@ -3,17 +3,19 @@
 read -p "Enter main directory name: " mainfolder
 mkdir $mainfolder
 cd $mainfolder
+
 read -p "Enter subdirectory name: " subfolder
 read -p "How man subdirectories? " numfolders
 read -p "Enter file name: " filename
 read -p "How many files per folder? " numfiles
+
 for i in $(seq 1 $numfolders)
-do
-mkdir ${subfolder}_${i}
-cd ${subfolder}_${i}
-for j in $(seq 1 $numfiles)
-do
-touch ${filename}_${j}.docx
-done
-cd ..
+  do
+  mkdir ${subfolder}_${i}
+  cd ${subfolder}_${i}
+  for j in $(seq 1 $numfiles)
+    do
+    touch ${filename}_${j}.docx
+  done
+  cd ..
 done
